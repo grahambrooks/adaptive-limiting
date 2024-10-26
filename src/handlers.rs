a327ed20e0ce622c5fd3cde54867161e31a17ddd
Graham Brooks <graham@grahambrooks.com>
2024-10-26 16:49:47 -0600

    started work on running in docker compose
    Took 2 hours 22 minutes

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,21 +1,27 @@
+use crate::models::ResponseTimeChangeRequest;
 use actix_web::{web, HttpResponse};
-use serde::Deserialize;
+use log::info;
 use std::sync::Mutex;
 
-use crate::models::ResponseTimeChangeRequest;
-use crate::lib::update_response_time;
-
 pub async fn hello(data: web::Data<crate::AppState>) -> HttpResponse {
+
     let response_time = data.response_time.lock().unwrap();
+    info!("/ requested delay: {}ms", *response_time);
     std::thread::sleep(std::time::Duration::from_millis(*response_time));
-    HttpResponse::Ok().json("hello")
+    HttpResponse::Ok().json(serde_json::json!({ "message": "hello" }))
 }
 
-pub async fn set_response_time(
-    data: web::Data<crate::AppState>,
-    req: web::Json<ResponseTimeChangeRequest>,
-) -> HttpResponse {
+pub async fn set_response_time(data: web::Data<crate::AppState>, req: web::Json<ResponseTimeChangeRequest>) -> HttpResponse {
     let mut response_time = data.response_time.lock().unwrap();
     *response_time = req.response_time;
     HttpResponse::Ok().finish()
+}
+
+pub struct AppState {
+    pub response_time: Mutex<u64>,
+}
+
+pub fn update_response_time(state: &AppState, new_time: u64) {
+    let mut response_time = state.response_time.lock().unwrap();
+    *response_time = new_time;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use actix_web::{web, App, HttpServer, Responder};
+use log::{info, LevelFilter};
 use serde::{Deserialize, Serialize};
+use simple_logger::SimpleLogger;
+use std::env;
 use std::sync::Mutex;
-
 mod handlers;
 mod models;
-mod lib;
 
 struct AppState {
     response_time: Mutex<u64>,
@@ -12,17 +13,23 @@ struct AppState {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
     let app_state = web::Data::new(AppState {
-        response_time: Mutex::new(0),
+        response_time: Mutex::new(100),
     });
+
+    let listen_port = env::var("LISTEN_PORT").unwrap_or_else(|_| "8080".to_string());
+
+    info!("Starting server on http://{}:{}", hostname::get().unwrap().to_string_lossy(), listen_port);
+
 
     HttpServer::new(move || {
         App::new()
             .app_data(app_state.clone())
-            .route("/hello", web::get().to(handlers::hello))
-            .route("/set_response_time", web::post().to(handlers::set_response_time))
+            .route("/", web::get().to(handlers::hello))
+            .route("/response/time", web::post().to(handlers::set_response_time))
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+        .bind(format!("127.0.0.1:{}", listen_port))?  // Bind
+        .run()
+        .await
 }
